5633d71f2389937e089425b6063910a0afa43e92
jcm2512 <joe.magalotti@gmail.com>
2024-08-04 13:20:46 +0900

    reduced script

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import os
-import mplfinance as mpf
-import pandas as pd
-import numpy as np
-import matplotlib.ticker as ticker
-from pytz import timezone
-from dotenv import load_dotenv
-
 from bitflyer_actions import (
     get_balance,
     get_open_ifd_orders,
@@ -14,39 +6,13 @@ from bitflyer_actions import (
     has_funds_for_order,
 )
 
-# from cryptocompare import fetch_ohlcv
+LIVE = False
 
-# from chart_actions import append_MCD, mpf_plot
-
-load_dotenv()
-
-OUTPUT_DIR = os.getenv("OUTPUT_DIR", "local_docs")
-os.makedirs(OUTPUT_DIR, exist_ok=True)
-
-LOCAL = False
-if OUTPUT_DIR == "local_docs":
-    LOCAL = True
-
-PENDING_ORDERS = os.path.join(OUTPUT_DIR, "pending_orders.csv")
-CSV_DATA = os.path.join(OUTPUT_DIR, "data.csv")
-HA_DATA = os.path.join(OUTPUT_DIR, "heikin_ashi.csv")
-SMA_DATA = os.path.join(OUTPUT_DIR, "sma_data.csv")
-EMA_DATA = os.path.join(OUTPUT_DIR, "ema_data.csv")
-EMA_DATA2 = os.path.join(OUTPUT_DIR, "ema_data_before.csv")
-DF_SIGNALS = os.path.join(OUTPUT_DIR, "df_signals.csv")
-
-JST = timezone("Asia/Tokyo")
-
-ENTRIES_PER_UPDATE = 50
-MAX_ENTRIES = 2000
-
-CHART_DURATION = 500  # 7 Days: 168 hours; 3 Days = 72
-
+# Initialise variables
+TEST_PRICE = None
 MIN_PRICE = 8900000
 MAX_PRICE = 10700000
 PRICE_INTERVAL = 200000
-
-MPF_PLOT = os.path.join(OUTPUT_DIR, "candlestick_plot.png")
 
 
 def grid_intervals(min=MIN_PRICE, max=MAX_PRICE, interval=PRICE_INTERVAL):
@@ -63,14 +29,9 @@ def is_open_order(amt, open_orders):
 
 
 if __name__ == "__main__":
-    LIVE = False
-
-    # Initialise variables
-    TEST_PRICE = None
-
     print("Starting Main script...")
     if not LIVE:
-        TEST_PRICE = 8925964
+        TEST_PRICE = 11500000
         ifd_orders = get_open_ifd_orders()
         print(f"ifd orders: {ifd_orders}")
 
@@ -84,12 +45,15 @@ if __name__ == "__main__":
     market_price = TEST_PRICE or get_current_market_price()
 
     buy_order_amt = find_closest_interval(market_price, intervals)
-    print(f"market price: {market_price} | buy amt: {buy_order_amt}")
+    print(f"intervals: {intervals}")
+    print(f"market price: {market_price}")
+    print(f"buy amt: {buy_order_amt}")
 
     if LIVE:
         if MIN_PRICE <= market_price <= MAX_PRICE:
             buy_order_amt = find_closest_interval(market_price, intervals)
             ifd_orders = get_open_ifd_orders()
+            print(f"IFD Orders: {ifd_orders}")
 
             if not is_open_order(buy_order_amt, ifd_orders):
                 if has_funds_for_order(market_price, get_balance("JPY")):
