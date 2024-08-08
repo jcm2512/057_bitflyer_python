ee99bf63ffa61f4d8b198608fef55d5d5213a9d6
jcm2512 <joe.magalotti@gmail.com>
2024-08-09 01:46:41 +0900

    ADDED: will cancel out of range orders

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from bitflyer_actions import (
     get_current_market_price,
     ifd_order,
     has_funds_for_order,
-    get_open_child_orders,
+    cancel_parent_order,
 )
 
 LIVE = True
 
 # Initialise variables
 TEST_PRICE = None
-MIN_PRICE = 8900000
+MIN_PRICE = 7500000
 MAX_PRICE = 10700000
 PRICE_INTERVAL = 200000
 
@@ -33,13 +33,12 @@ def is_open_order(amt, open_orders):
 
 if __name__ == "__main__":
     print("Starting Main script...")
-    if not LIVE:
-        TEST_PRICE = 11500000
-        ifd_orders = get_open_ifd_orders()
-        print(f"ifd orders: {ifd_orders}")
+    print("")
 
     grid_interval = PRICE_INTERVAL
     intervals = grid_intervals()
+    ifd_orders = get_open_ifd_orders()
+    # print(f"ACTIVE Orders: {ifd_orders}")
 
     # TODO: Get high and low for past 90 days to determine Min and Max price
     # Min price should be at least 1 interval above the lowest price
@@ -48,36 +47,49 @@ if __name__ == "__main__":
     market_price = TEST_PRICE or get_current_market_price()
 
     buy_order_amt = find_closest_interval(market_price, intervals)
-    print(f"intervals: {intervals}")
+
     print(f"market price: {market_price}")
     print(f"buy amt: {buy_order_amt}")
 
+    # Remove IFD orders that are out of current range
+    # -----------------------------------------------
+    bottom_range = buy_order_amt - PRICE_INTERVAL
+    print(f"bottom range: {bottom_range}")
+    print("")
+    open_order = [
+        {
+            "parent_order_acceptance_id": order["parent_order_acceptance_id"],
+            "price": order["price"],
+        }
+        for order in ifd_orders
+        if order["price"] == bottom_range
+    ]
+    for order in open_order:
+        print(f"CANCELING ORDER")
+        print("----------")
+        print(f"price: {int(order["price"])}")
+        print(f"id: {order["parent_order_acceptance_id"]}")
+        cancel_parent_order(order["parent_order_acceptance_id"])
+        print("----------")
+
+
     if LIVE:
-        ifd_orders = get_open_ifd_orders()
-
-        # Remove orders out of current price range
-        print("GETTING CURRENT ACTIVE IDF ORDERS...")
-        for order in ifd_orders:
-            child_orders = get_open_child_orders(order["parent_order_id"])
-            # NOTE: Each IFD order has 2 orders.
-            # List parent orders if both child orders are active
-            if len(child_orders) == 2:
-                print(order["parent_order_acceptance_id"])
-                # TODO: If open child order is still out of current price range CANCEL the order
-
+        # Check if price band is within range
         if (MIN_PRICE - PRICE_INTERVAL) <= market_price <= MAX_PRICE:
             buy_order_amt = find_closest_interval(market_price, intervals)
 
+            # Check if current price band is a vacant order
             if not is_open_order(buy_order_amt, ifd_orders):
+
+                # Check balance and create IFD order
                 if has_funds_for_order(market_price, get_balance("JPY")):
                     ifd_order(buy_order_amt, grid_interval)
+
                 else:
                     print("INSUFFICIENT FUNDS")
             else:
                 print(f"Order:{buy_order_amt} exists \n EXITING...")
         else:
             print("PRICE IS OUT OF RANGE")
-
-        print(f"IFD Orders: {ifd_orders}")
 
     print("DONE")
